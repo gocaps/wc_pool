d80f997ab96beec90a2dbe9d58cf82a08bc6278e
Dave Kennedy <david.kennedy@ubc.ca>
2014-05-29 05:48:11 -0700

    added updating message added standings html page

--- index.php
+++ index.php
@@ -31,12 +31,18 @@ table.imagetable td {
 <?php
 
 require_once('wc_pool.php');
-echo '<a href="#groups">See Groups</a>';
 
-$wc_pool = new WC_Pool();
-$wc_pool->calculateStandings();
-$wc_pool->displayStandings();
-$wc_pool->displayTeams();
-$wc_pool->displayGroups();
-
+if(UPDATING) {
+  echo '<h3>HOLD YOUR HORSES!</h3>';
+  echo '<p>I\'m doing some standings updates. Check this out, and by the time you\'re done watching<br />you should be able to refresh your page and see the up to date standings. ~Dave</p>';
+  echo '<script height="349px" width="620px" src="http://player.ooyala.com/iframe.js#pbid=2d940881487848159b3b99a3ae8125b9&ec=BnYWMybjqe_Ot8yd9ynDnwXukThl2obX"></script>';
+}
+else {
+  echo '<a href="#groups">See Groups</a>';
+  $wc_pool = new WC_Pool();
+  $wc_pool->calculateStandings();
+  $wc_pool->displayStandings();
+  $wc_pool->displayTeams();
+  $wc_pool->displayGroups();
+}
 ?>
